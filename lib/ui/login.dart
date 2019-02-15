625c565c1ce10d1c9d5be9209e81f2808f076b9f
itisowen <60070105@kmitl.ac.th>
2019-02-16 02:55:14 +0700

    update

--- lib/ui/login.dart
+++ lib/ui/login.dart
@@ -2,16 +2,15 @@ import 'package:flutter/material.dart';
 import './home.dart';
 import './regis.dart';
 
-class Login_Page extends StatefulWidget{
+class Login_Page extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
     // TODO: implement createState
     return Login_PageState();
   }
-
 }
 
-class Login_PageState extends State<StatefulWidget>{
+class Login_PageState extends State<StatefulWidget> {
   final userid = TextEditingController();
   final password = TextEditingController();
   final _scaffoldKey = GlobalKey<ScaffoldState>();
@@ -19,72 +18,77 @@ class Login_PageState extends State<StatefulWidget>{
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-        return Scaffold(
-          key: _scaffoldKey,
-          body: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 35),
-            child: Center(
+    return Scaffold(
+        key: _scaffoldKey,
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 35),
+          child: Center(
               child: ListView(
-                children:<Widget>[
-                  Padding(
-                    padding: const EdgeInsets.only(top: 25, bottom: 15),
-                    child: Image.asset(
-                      'img/2.jpg',
-                      height: 200,
-                    ),
-                  ),
-                  TextFormField(
-                    style: TextStyle(
-                      fontSize: 18,
-                      color: Colors.grey
-                    ),
-                    controller: userid,
-                    decoration: InputDecoration(
-                      hintText: 'User Id', prefixIcon: Icon(Icons.person, size: 30,) 
-                    ),
-                  ),
-                  TextFormField(
-                    style: TextStyle(
-                      fontSize: 18,
-                      color: Colors.grey
-                    ),
-                    controller: password,
-                    obscureText: true,
-                    decoration: InputDecoration(
-                      hintText: 'Password', prefixIcon: Icon(Icons.lock, size: 30,) 
-                    ),
-                  ),
-                  RaisedButton(
-                    child: Text('LOGIN'),
-                    onPressed: (){
-                      if(userid.text.isEmpty || password.text.isEmpty){
-                        final snackBar = SnackBar(
-                          content: Text('กรุณาระบุ user or password'),
-                        );
-                        _scaffoldKey.currentState.showSnackBar(snackBar);
-                      }else if(userid.text == 'admin' && password.text == 'admin'){
-                        final snackBar = SnackBar(
-                          content: Text('user or password ไม่ถูกต้อง'),
-                        );
-                        _scaffoldKey.currentState.showSnackBar(snackBar);
-                      }else{
-                      Navigator.push(context, MaterialPageRoute(builder: (context)=>Home_Page()));
-                      }
-                    },
-                    ),
-                    Align(
-                      alignment: Alignment.topRight,
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.only(top: 25, bottom: 15),
+                child: Image.asset(
+                  'img/2.jpg',
+                  height: 170,
+                ),
+              ),
+              TextFormField(
+                style: TextStyle(fontSize: 18, color: Colors.grey),
+                controller: userid,
+                decoration: InputDecoration(
+                    hintText: 'User ID',
+                    prefixIcon: Icon(
+                      Icons.person,
+                      size: 30,
+                    )),
+              ),
+              TextFormField(
+                style: TextStyle(fontSize: 18, color: Colors.grey),
+                controller: password,
+                obscureText: true,
+                decoration: InputDecoration(
+                    hintText: 'Password',
+                    prefixIcon: Icon(
+                      Icons.lock,
+                      size: 30,
+                    )),
+              ),
+              RaisedButton(
+                child: Text('LOGIN'),
+                onPressed: () {
+                  if (userid.text.isEmpty || password.text.isEmpty) {
+                    final snackBar = SnackBar(
+                      content: Text('กรุณาระบุ user or password'),
+                    );
+                    _scaffoldKey.currentState.showSnackBar(snackBar);
+                  } else if (userid.text == 'admin' &&
+                      password.text == 'admin') {
+                    final snackBar = SnackBar(
+                      content: Text('user or password ไม่ถูกต้อง'),
+                    );
+                    _scaffoldKey.currentState.showSnackBar(snackBar);
+                  } else {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => Home_Page()));
+                  }
+                },
+              ),
+              Align(
+                  alignment: Alignment.topRight,
+                  child: ButtonTheme(
+                      padding: new EdgeInsets.all(0.0),
                       child: FlatButton(
-                      child: Text('Register New Account'),
-                      onPressed: (){
-                        Navigator.push(context, MaterialPageRoute(builder: (context)=>Regis_Page()));
-                      },
-                      )
-                    )
+                        child: Text('Register New Account'),
+                        textColor: Theme.of(context).primaryColor,
+                        onPressed: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => Regis_Page()));
+                        },
+                      ))),
             ],
-        )
-      ),
-          ));
+          )),
+        ));
   }
-
 }
